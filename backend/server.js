07fae66445de8a3bce10e3682fcb77c46b705bb3
Alisson Rubens <alissonrubens.s@gmail.com>
2026-01-11 20:02:23 -0300

    fix: update CORS configuration for improved security and clarity

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,14 @@ import AuthRoutes from "./Routes/auth.routes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["*"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.options("*", cors());
 app.use(express.json());
 
 app.use("/users", UsersRoutes);
